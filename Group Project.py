935ef5bcdbaab6394f7bbba70e0b031337742310
OliverMardle <olivermardle123@btinternet.com>
2017-04-09 18:59:50 +0100

    Update Group Project.py
    Logic Functions all work except NOT, ill fix it in a minute

--- Group Project.py
+++ Group Project.py
@@ -2,7 +2,6 @@
 # Edited By Dawid S
 from tkinter import *
 from tkinter import ttk
-import random
 
 window = Tk()
 window.resizable(width=False, height=False)
@@ -11,6 +10,7 @@ window.title('Logic Of Woe')
 Count = 7
 Tabs = ['AND','NAND','NOR','NOT','OR','XNOR','XOR']
 Images = ['AND.gif','NAND.gif','NOR.gif','NOT.gif','OR.gif','XNOR.gif','XOR.gif']
+Labels = []
 Names = []
 
 F1 = Frame(window)
@@ -33,10 +33,62 @@ def Check(window):
                 Variables[i].set(0)
         NB.tab(NB.index(NB.select()), state = 'disabled')
         NB.select(Names[NB.index(NB.select())])
+
+def Tab1(window, x):
+        if Var1.get() == 1 and Var2.get() == 1:
+                Labels[x].configure(text = '1')
+        else:
+                Labels[x].configure(text = '0')
+        window.update()
         
+def Tab2(window, x):
+        if Var1.get() == 1 and Var2.get() == 1:
+                Labels[x].configure(text = '0')
+        else:
+                Labels[x].configure(text = '1')
+        window.update()
+        
+def Tab3(window, x):
+        if Var1.get() == 0 and Var2.get() == 0:
+                Labels[x].configure(text = '1')
+        else:
+                Labels[x].configure(text = '0')
+        window.update()
+                        
+def Tab4(window, x):
+        if Var1.get() == 1 and Var2.get() == 1:
+                Labels[x].configure(text = '1')
+        else:
+                Labels[x].configure(text = '0')
+        window.update()
+                        
+def Tab5(window, x):
+        if Var1.get() == 0 and Var2.get() == 0:
+                Labels[x].configure(text = '0')
+        else:
+                Labels[x].configure(text = '1')
+        window.update()
+                        
+def Tab6(window, x):
+        if (Var1.get() == 1 and Var2.get() == 1) or (Var1.get() == 0 and Var2.get() == 0):
+                Labels[x].configure(text = '1')
+        else:
+                Labels[x].configure(text = '0')
+        window.update()
+                        
+def Tab7(window, x):
+        if (Var1.get() == 1 and Var2.get() == 1) or (Var1.get() == 0 and Var2.get() == 0):
+                Labels[x].configure(text = '0')
+        else:
+                Labels[x].configure(text = '1')
+        window.update()
+
+TabFunctions = [Tab1, Tab2, Tab3, Tab4, Tab5, Tab6, Tab7]
+
 def CreateGUI(Count):
         for i in range(0, Count):
                 Names.append('T' + str(i + 1))
+                Labels.append('O' + str(i + 1))
         for i in range(0, Count):
                 Names[i] = ttk.Frame(window)
                 NB.add(Names[i], text = Tabs[i])
@@ -47,7 +99,7 @@ def CreateGUI(Count):
                 R2 = ttk.Checkbutton(Names[i], variable = Var2)
                 P1 = ttk.Label(Names[i], image = Photo)
                 P1.image = Photo
-                L1 = ttk.Label(Names[i], text = '0')
+                Labels[i] = ttk.Label(Names[i], text = '0')
                 F2 = ttk.Frame(Names[i])
                 L2 = ttk.Label(F2, text = '1  |  1  |')
                 L3 = ttk.Label(F2, text = '1  |  0  |')
@@ -62,7 +114,7 @@ def CreateGUI(Count):
                 R1.grid(row = 1, column = 1, rowspan = 2)
                 R2.grid(row = 3, column = 1, rowspan = 2)
                 P1.grid(row = 1, column = 2, rowspan = 4, columnspan = 3)
-                L1.grid(row = 1, column = 5, rowspan = 4)
+                Labels[i].grid(row = 1, column = 5, rowspan = 4)
                 B1.grid(row = 5, column = 3, rowspan = 4, columnspan = 3)
                 F2.grid(row = 5, column = 1, columnspan = 2)
                 L2.grid(row = 1, column = 1)
@@ -77,13 +129,7 @@ def CreateGUI(Count):
         NB.pack(fill = BOTH)
         while True:
                 x = NB.index(NB.select())
-                if x == 0:
-                        print(Var1.get(), Var2.get())
-                        if Var1.get() == 1 and Var2.get() == 1:
-                                L1.configure(text = '1')
-                        else:
-                                L1.configure(text = '0')
-                        window.update()
+                TabFunctions[x](window, x)
                 window.update()
 CreateGUI(Count)
 
